b1620017c256ab6cf3c573f39b5dc075b30b3251
Jayesh5553 <jayeshwankhede17712@gmail.com>
2026-01-02 13:44:46 +0530

    array

---
+++ Day 9 arraymethod/script.js
@@ -0,0 +1,10 @@
+let name1=["Haresh","Jayesh","Om","Om"]
+let name2=["diya","Monica","ABC"]
+let s=name1.concat(name2)
+console.log(s);
+console.log(s.indexOf("Om"));
+console.log(s.lastIndexOf("Om"));
+console.log(s.includes("Jayesh"));
+console.log(s.includes("Om"));
+
+
