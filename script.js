7f811ada1ba4e731d06532f6c047f9718bb0127a
Jayeshwankhede <160487820+Jayesh5553@users.noreply.github.com>
2026-01-02 13:37:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// function first(arr,count){
+//     let ans=[]
+//     if(count==undefined){
+//         return arr[0]
+//     }else{
+//         let end;
+//         if(count>arr.length){
+//             end=arr.length
+//         }else{
+//             end=count
+//         }
+//         for(let i=0;i<=end-1;i++){
+//             ans.push(arr[i])
+//         }
+//         return ans
+//     }
+// }
+// let a=first([1,2,3],2)
+// console.log(a);
